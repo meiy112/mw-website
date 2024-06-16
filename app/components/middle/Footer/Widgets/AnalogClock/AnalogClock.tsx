8cf4f4351f1b86729709db3aa715603288274118
maggie <maggie.weng112@gmail.com>
2024-06-16 14:35:20 -0700

    finished clock

--- app/components/middle/Footer/Widgets/AnalogClock/AnalogClock.tsx
+++ app/components/middle/Footer/Widgets/AnalogClock/AnalogClock.tsx
@@ -49,11 +49,28 @@ export default function AnalogClock() {
     };
   }, [time]);
 
+  useEffect(() => {
+    let dials = document.getElementsByClassName("dials");
+    let clock = document.getElementById("clock");
+
+    if (clock) {
+      for (let i = 1; i < 60; i++) {
+        clock.innerHTML += `<div class="${styles.dials} dials"></div>`;
+
+        const dial = dials[i] as HTMLElement;
+        dial.style.transform = `rotate(${6 * i}deg)`;
+      }
+    }
+  }, []);
+
   return (
     <div
-      className={`${styles.clock} relative flex items-center justify-center w-[10.125vw] aspect-square rounded-[23px] glass-container`}
+      className={`${styles.clock} relative flex items-center justify-center w-[150px] aspect-square rounded-[23px] glass-container`}
     >
-      <div className="flex items-center justify-center w-[80%] aspect-square rounded-[50%] bg-black relative text-center">
+      <div
+        id="clock"
+        className="flex items-center justify-center w-[80%] aspect-square rounded-[50%] bg-black relative text-center"
+      >
         <div id="secondHand" className={`${styles.hand} ${styles.second}`}>
           <div className={`${styles.secondHand}`} />
         </div>
@@ -68,45 +85,58 @@ export default function AnalogClock() {
           </div>
         </div>
         <div
-          className={`${styles.center} flex items-center justify-center w-[6px] h-[6px] rounded-[50%] bg-black`}
+          className={`${styles.center} flex items-center justify-center w-[3px] h-[3px] rounded-[50%] bg-black`}
         ></div>
+        <div className={`${styles.dials} dials`}></div>
       </div>
 
       <span className={`${styles.number}`}>
-        <span>12</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>12</span>
       </span>
       <span className={`${styles.number} ${styles.number1}`}>
-        <span>1</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>1</span>
       </span>
       <span className={`${styles.number} ${styles.number2}`}>
-        <span>2</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>2</span>
       </span>
       <span className={`${styles.number} ${styles.number3}`}>
-        <span>3</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>3</span>
       </span>
       <span className={`${styles.number} ${styles.number4}`}>
-        <span>4</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>4</span>
       </span>
       <span className={`${styles.number} ${styles.number5}`}>
-        <span>5</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>5</span>
       </span>
       <span className={`${styles.number} ${styles.number6}`}>
-        <span>6</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>6</span>
       </span>
       <span className={`${styles.number} ${styles.number7}`}>
-        <span>7</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>7</span>
       </span>
       <span className={`${styles.number} ${styles.number8}`}>
-        <span>8</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>8</span>
       </span>
       <span className={`${styles.number} ${styles.number9}`}>
-        <span>9</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>9</span>
       </span>
       <span className={`${styles.number} ${styles.number10}`}>
-        <span>10</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>10</span>
       </span>
       <span className={`${styles.number} ${styles.number11}`}>
-        <span>11</span>
+        <span className={`${styles.thickLine}`}></span>
+        <span className={`${styles.numberInner}`}>11</span>
       </span>
     </div>
   );
