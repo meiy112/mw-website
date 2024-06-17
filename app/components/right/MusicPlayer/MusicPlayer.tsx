035926749320a143bfc0e5cd2ba9411393e3156d
maggie <maggie.weng112@gmail.com>
2024-06-17 00:11:03 -0700

    music player works

--- app/components/right/MusicPlayer/MusicPlayer.tsx
+++ app/components/right/MusicPlayer/MusicPlayer.tsx
@@ -1,12 +1,13 @@
 import { IoMusicalNotes } from "react-icons/io5";
 import styles from "./MusicPlayer.module.css";
-import { IoIosPlay } from "react-icons/io";
+import { IoIosPause, IoIosPlay } from "react-icons/io";
 import { useDroppable } from "@dnd-kit/core";
 import { useDragContext } from "../../context/DragContext";
 import { AnimatePresence, motion } from "framer-motion";
 import { FiUpload } from "react-icons/fi";
 import getCurrentTitle from "./getCurrentTitle";
 import getCurrentAuthor from "./getCurrentAuthor";
+import { useEffect, useState } from "react";
 
 export default function MusicPlayer() {
   const { isOver, setNodeRef } = useDroppable({
@@ -21,43 +22,145 @@ export default function MusicPlayer() {
 
   const { currentChildId, parent, draggables, isDragging } = dragContext;
 
+  const playButton = <IoIosPlay className="text-black" size={20} />;
+  const loader = <span className={styles.loader}></span>;
+  const pauseButton = <IoIosPause className="text-black" size={20} />;
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [buttonChild, setButtonChild] = useState(playButton);
+
+  const duckAudio = new Audio("./audio/duck.mp3");
+  const acnhAudio = new Audio("./audio/acnh.mp3");
+  const yhxAudio = new Audio("./audio/yhx.mp3");
+  const pianoAudio = new Audio("./audio/duck.mp3");
+
+  const [audio, setAudio] = useState(acnhAudio);
+
+  useEffect(() => {
+    if (audio) {
+      isPlaying ? audio.play() : audio.pause();
+    }
+  }, [isPlaying]);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+    setButtonChild(pauseButton);
+  };
+  const handlePause = () => {
+    setIsPlaying(false);
+    setButtonChild(playButton);
+  };
+
+  const handleButtonPress = () => {
+    if (isPlaying) {
+      handlePause();
+    } else {
+      handlePlay();
+    }
+  };
+
+  const handleLoad = () => {
+    setIsPlaying(false);
+    setButtonChild(loader);
+    setTimeout(() => {
+      handlePlay();
+    }, 1500);
+  };
+
+  useEffect(() => {
+    if (currentChildId != null) {
+      if (currentChildId == "draggable0") {
+        setAudio(acnhAudio);
+      } else if (currentChildId == "draggable1") {
+        setAudio(yhxAudio);
+      } else if (currentChildId == "draggable2") {
+        setAudio(pianoAudio);
+      } else {
+        setAudio(duckAudio);
+      }
+      handleLoad();
+    } else {
+      handlePause();
+    }
+  }, [currentChildId]);
+
+  useEffect(() => {
+    if (isDragging && isPlaying) {
+      handlePause();
+    }
+  }, [isDragging]);
+
   const getCurrentChild = () => {
-    if (currentChildId === "draggable0" && parent === "droppable") {
-      return (
-        <>
-          {draggables[0]}
-          <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black">
-            <IoMusicalNotes size={22} className="opacity-[0.7]" />
-          </div>
-        </>
-      );
-    } else if (currentChildId === "draggable1" && parent === "droppable") {
-      return (
-        <>
-          {draggables[1]}
-          <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black">
-            <IoMusicalNotes size={22} className="opacity-[0.7]" />
-          </div>
-        </>
-      );
-    } else if (currentChildId === "draggable2" && parent === "droppable") {
-      return (
-        <>
-          {draggables[2]}
-          <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black">
-            <IoMusicalNotes size={22} className="opacity-[0.7]" />
-          </div>
-        </>
-      );
-    } else if (currentChildId === "draggable3" && parent === "droppable") {
-      return (
-        <>
-          {draggables[3]}
-          <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black">
-            <IoMusicalNotes size={22} className="opacity-[0.7]" />
-          </div>
-        </>
-      );
+    if (parent === "droppable") {
+      if (currentChildId === "draggable0") {
+        return (
+          <>
+            <div
+              className={
+                isDragging
+                  ? ""
+                  : isPlaying
+                  ? styles.rotate
+                  : `${styles.rotate} ${styles.paused}`
+              }
+            >
+              {draggables[0]}
+            </div>
+            <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black"></div>
+          </>
+        );
+      } else if (currentChildId === "draggable1") {
+        return (
+          <>
+            <div
+              className={
+                isDragging
+                  ? ""
+                  : isPlaying
+                  ? styles.rotate
+                  : `${styles.rotate} ${styles.paused}`
+              }
+            >
+              {draggables[1]}
+            </div>
+            <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black"></div>
+          </>
+        );
+      } else if (currentChildId === "draggable2") {
+        return (
+          <>
+            <div
+              className={
+                isDragging
+                  ? ""
+                  : isPlaying
+                  ? styles.rotate
+                  : `${styles.rotate} ${styles.paused}`
+              }
+            >
+              {draggables[2]}
+            </div>
+            <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black"></div>
+          </>
+        );
+      } else if (currentChildId === "draggable3") {
+        return (
+          <>
+            <div
+              className={
+                isDragging
+                  ? ""
+                  : isPlaying
+                  ? styles.rotate
+                  : `${styles.rotate} ${styles.paused}`
+              }
+            >
+              {draggables[3]}
+            </div>
+            <div className="z-0 absolute flex justify-center items-center aspect-square h-[67px] rounded-[50%] bg-black"></div>
+          </>
+        );
+      }
     } else {
       return (
         <div
@@ -112,8 +215,9 @@ export default function MusicPlayer() {
       </div>
       <button
         className={`${styles.button} mr-[5%] h-[32px] flex justify-center items-center aspect-square rounded-[50%] bg-white`}
+        onClick={handleButtonPress}
       >
-        <IoIosPlay className="text-black" size={20} />
+        {buttonChild}
       </button>
     </div>
   );
