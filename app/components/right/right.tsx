a55bb6f8df2db4e4347a809dea2168cb62a90d9f
meiy112 <maggie.weng112@gmail.com>
2024-11-26 22:43:13 -0800

    Loading Screen Z Index

--- app/components/right/right.tsx
+++ app/components/right/right.tsx
@@ -4,7 +4,7 @@ import MusicPlayer from "./MusicPlayer/MusicPlayer";
 
 export default function Right() {
   return (
-    <div className="gap-y-[15px] right w-[100%] h-[100%] fixed flex flex-col items-start justify-start">
+    <div className="gap-y-[15px] right h-[100vh] pt-[4.2em] w-[100%] sticky top-0 flex flex-col items-start justify-start">
       <Recommendations />
       <MusicPlayer />
     </div>
