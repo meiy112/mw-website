f2c7d7be20200670bd8a58478dab09aedd13440b
meiy112 <maggie.weng112@gmail.com>
2025-02-25 12:16:42 -0800

    Pantry planner post

--- app/posts/projects/ProjectContent.tsx
+++ app/posts/projects/ProjectContent.tsx
@@ -3,10 +3,12 @@ import { Mangoose } from "./Mangoose";
 import { StudyShark } from "./StudyShark";
 import { InterviewAI } from "./InterviewAI";
 import { GCodePathfinder } from "./GCodePathfinder";
+import { PantryPlanner } from "./PantryPlanner";
 
 export const ProjectContent: PostData[] = [
   GCodePathfinder,
   Mangoose,
   InterviewAI,
   StudyShark,
+  PantryPlanner,
 ];
